f93c7a225ce175de605052ba819806cac5a3413f
DRCOFFEE\zhaowc <1647208238@qq.com>
2023-09-06 13:09:04 +0800

    feat: add func share.Manager.NewWithRepo

--- share/manager.go
+++ share/manager.go
@@ -2,6 +2,7 @@ package share
 
 import (
 	"github.com/gin-gonic/gin"
+	"reflect"
 	"weicai.zhao.io/consts"
 	"weicai.zhao.io/gormx"
 	"weicai.zhao.io/repox"
@@ -23,4 +24,23 @@ func (m *Manager) GormManager() *gormx.Manager {
 
 func (m *Manager) GinResponse(ctx *gin.Context) responsex.Response {
 	return responsex.NewGinJsonResponse(m.mode, ctx)
+}
+
+func (m *Manager) NewWithRepo(target any) {
+	// 初始化
+	if target == nil {
+		return
+	}
+
+	v := reflect.ValueOf(target)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	for i := 0; i < v.NumField(); i++ {
+		repo := m.repoManager.Load(v.Field(i).Type())
+		if repo != nil {
+			v.Field(i).Set(reflect.ValueOf(repo))
+		}
+	}
 }
